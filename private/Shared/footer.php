21cda321136a5166ead55ebdcb4f9b05b8b65072
Xavier Anthony <Xabanthony@gmail.com>
2025-02-28 12:40:32 +1300

    first commit

---
+++ private/Shared/footer.php
@@ -0,0 +1,40 @@
+        <footer>
+            <div class="align6">
+                <img src="<?php echo WWW_ROOT . "/images/rosmini_logo/logo-300x259.png";?>" alt="Rosmini Logo" id="Rosmini_Logo2">
+            </div>
+            <br>
+            <div class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div class="align_footer2">
+                    <ul class="align_footer">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="<?php echo WWW_ROOT . "/home//index.php";?>">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/staff/index.php";?>">Staff</a>
+                        </li>
+                    </ul>
+                    <ul class="align_footer">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/sports/index.php";?>">Sports</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/contacts/index.php";?>">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/news/index.php";?>">News</a>
+                        </li>
+                    </ul>
+                    <ul class="align_footer">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/student_support/index.php";?>">Student Support</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo WWW_ROOT . "/home/learning_areas/index.php";?>">Learning Areas</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            &copy; <?php echo date('Y'); ?> Rosmini College
+        </footer>
+    </body>
+</html>
